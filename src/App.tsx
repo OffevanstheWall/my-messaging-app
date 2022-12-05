f042466122e4134534cc28c2a4d5bd49f0a42ae4
offevansthewall <offevansthewall@outlook.com>
2022-12-05 16:09:29 -0600

    simple messaging app using react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+interface Message {
+  username: string;
+  message: string;
+}
 
 function App() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [activeUser, setActiveUser] = useState<string>("user1");
+  const [textareaValue, setTextareaValue] = useState<string>("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="root">
+      <select
+        className="select-user"
+        onChange={(e) => {
+          setActiveUser(e.currentTarget.value);
+        }}
+      >
+        <option value="user1">user1</option>
+        <option value="user2">user2</option>
+      </select>
+      <div className="messages">
+        {messages.map((message) => {
+          return (
+            <div className={message.username === activeUser ? "me" : "other"}>
+              {message.message}
+            </div>
+          );
+        })}
+      </div>
+      <div className="user-inputs">
+        <textarea
+          className="input-message-text"
+          value={textareaValue}
+          onChange={(e) => {
+            setTextareaValue(e.target.value);
+          }}
+        ></textarea>
+        <button
+          className="send-button"
+          onClick={() => {
+            const message: Message = {
+              username: activeUser,
+              message: textareaValue,
+            };
+            setMessages([message, ...messages]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Send
+        </button>
+      </div>
     </div>
   );
 }
