074cfb901a05fa991527d454415b9395b71afef8
offevansthewall <offevansthewall@outlook.com>
2022-12-08 20:22:39 -0600

    send on enter key press and shift enter for multiline

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 interface Message {
@@ -11,17 +11,31 @@ function App() {
   const [activeUser, setActiveUser] = useState<string>("user1");
   const [textareaValue, setTextareaValue] = useState<string>("");
 
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
   useEffect(() => {
     const handler = () => {
       // We execute the same script as before
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty("--vh", `${vh}px`);
     };
+    inputRef.current?.focus();
     window.addEventListener("resize", handler);
     return () => {
       window.removeEventListener("resize", handler);
     };
   }, []);
+
+  function send() {
+    console.log(textareaValue);
+    setTextareaValue("");
+    inputRef.current?.focus();
+    const message: Message = {
+      username: activeUser,
+      message: textareaValue,
+    };
+    setMessages([message, ...messages]);
+  }
 
   return (
     <div className="root">
@@ -38,13 +52,21 @@ function App() {
         {messages.map((message) => {
           return (
             <div className={message.username === activeUser ? "me" : "other"}>
-              {message.message}
+              {message.message.split("\n").map((message) => (
+                <div>{message}</div>
+              ))}
             </div>
           );
         })}
       </div>
       <div className="user-inputs">
         <textarea
+          onKeyUp={(e) => {
+            if (e.key.toLowerCase() === "enter" && !e.shiftKey) {
+              send();
+            }
+          }}
+          ref={inputRef}
           className="input-message-text"
           value={textareaValue}
           onChange={(e) => {
@@ -53,12 +75,8 @@ function App() {
         ></textarea>
         <button
           className="send-button"
-          onClick={() => {
-            const message: Message = {
-              username: activeUser,
-              message: textareaValue,
-            };
-            setMessages([message, ...messages]);
+          onClick={(e) => {
+            send();
           }}
         >
           Send
